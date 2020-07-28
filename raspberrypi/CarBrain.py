cfcae361d6ed0ca38fae1079a3ee504fc22472c6
Adithya Kothapalli <adithya.raju7@gmail.com>
2020-07-29 00:01:58 +0530

    Update CarBrain.py

--- raspberrypi/CarBrain.py
+++ raspberrypi/CarBrain.py
@@ -5,12 +5,8 @@ import tensorflow as tf
 import threading
 import time
 
-"""
-Predictions
-"""
 class CarBrain(object):
 	def __init__(self):
-		#Utils.print_log("Init. Model",1)
 		print('Init carbrain')
 		self.model = load_model('keras_model_0.001_learning_rate_32_batch_size_Adam_optimizer_88.0_acc.model')
 		print('Init carbrain model loaded')
@@ -18,10 +14,8 @@ class CarBrain(object):
 		print('Init carbrain done')
 			
 	def get_direction(self,_image):
-		#t = time.time()
 		
 		res =  self.model.predict(_image, batch_size=1)
-		#t = Utils.print_log('Model Predict',3,t)
 		
 		#print(res)
 		max_val = 0
